ad4192b73bd099be58c3bf297216cd32cabf029c
Brian Gamble <bjgamble@gmail.com>
2011-04-30 23:53:42 -0500

    moved determine_amount to Movie

--- movie.rb
+++ movie.rb
@@ -9,4 +9,19 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+
+  def determine_amount(days_rented)
+    rental_amount = 0
+    case @price_code
+      when REGULAR
+        rental_amount += 2
+        rental_amount += (days_rented - 2) * 1.5 if days_rented > 2
+      when NEW_RELEASE
+        rental_amount += days_rented * 3
+      when CHILDRENS
+        rental_amount += 1.5
+        rental_amount += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    rental_amount
+  end
 end
