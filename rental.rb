b0fd53b0e2fdec88c8d8ba5446df9525e0d69001
Brian Gamble <bjgamble@gmail.com>
2011-04-30 23:39:51 -0500

    moved determine_amount to Rental

--- rental.rb
+++ rental.rb
@@ -8,4 +8,19 @@ class Rental
   def title
     @movie.title
   end
+
+  def determine_amount
+    rental_amount = 0
+    case movie.price_code
+      when Movie::REGULAR
+        rental_amount += 2
+        rental_amount += (days_rented - 2) * 1.5 if days_rented > 2
+      when Movie::NEW_RELEASE
+        rental_amount += days_rented * 3
+      when Movie::CHILDRENS
+        rental_amount += 1.5
+        rental_amount += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    rental_amount
+  end
 end
--- statement.rb
+++ statement.rb
@@ -45,25 +45,10 @@ class Statement
   end
 
   def rental_line(rental)
-    rental_amount = determine_amount(rental)
+    rental_amount = rental.determine_amount
     @frequent_renter_points += determine_frequent_renter_points(rental)
     @total_amount += rental_amount
     format_rental_line(rental, rental_amount)
-  end
-
-  def determine_amount(rental)
-    rental_amount = 0
-    case rental.movie.price_code
-      when Movie::REGULAR
-        rental_amount += 2
-        rental_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-        rental_amount += rental.days_rented * 3
-      when Movie::CHILDRENS
-        rental_amount += 1.5
-        rental_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
-    rental_amount
   end
 
   def determine_frequent_renter_points(rental)
